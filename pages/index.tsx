6f41fef748615522c9bef5ecea43ed0f65adb35e
kekelberi <alexei.gaponov@gmail.com>
2022-06-25 17:14:33 +0300

    save

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,9 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = ({ data }: any) => {
-  console.log(data)
+  if (!data) {
+    return <h1>Nothing data</h1>
+  }
 
   return (
     <div>
@@ -13,7 +15,7 @@ const Home: NextPage = ({ data }: any) => {
       </Head>
 
       <div>
-        {data.map((el: any) => (
+        {data?.map((el: any) => (
           <h1 key={el.id}>{el.title}</h1>
         ))}
       </div>
@@ -23,12 +25,10 @@ const Home: NextPage = ({ data }: any) => {
 
 export default Home
 
-export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:5000/api/users')
-  const data = await res.json()
-  return {
-    props: {
-      data,
-    },
-  }
-}
+// export const getServerSideProps = async () => {
+//   const res = await fetch('http://localhost:5000/api/users')
+//   const data = await res?.json() || {}
+//   return {
+//     data
+//   }
+// }
